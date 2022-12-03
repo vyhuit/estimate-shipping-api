d7b82054209462ae415ecbb4f5eee3f73721e037
Vo Yen Hoang <vyhuit.m@gmail.com>
2022-12-03 11:40:50 +0700

    folder structure

--- src/config/database/mongo/config.service.ts
+++
@@ -1,6 +0,0 @@
-import {MongooseModule} from "@nestjs/mongoose"
-
-const mongodbConnect = MongooseModule.forRoot(process.env.ROOT_ROOT_DATABASE_URL + process.env.ESTIMATE_DB_PRODUCT || "mongodb+srv://hoang:Voyenhoang1998@cluster0.cwg4pt0.mongodb.net/test")
-
-export {
-  mongodbConnect}
